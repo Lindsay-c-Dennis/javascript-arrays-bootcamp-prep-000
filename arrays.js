0a071a687d811cc9ed39f402c416b6a6ef92c840
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2017-12-02 22:22:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 var chocolateBars = ["snickers", 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
-  
+  return [element, ...array]
 }
-function destructivelyAddElementToBeginningOfArray
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
