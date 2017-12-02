1aba6ccb2a7c85b6d57f2debe7cb36fa2431f7ef
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2017-12-02 22:31:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+function addElementToEndOfArray (element, array) {
+  return [...array, element]
+}
+function destructivelyAddElementToEndOfArray (element, array) {
+  array.push(element)
+  return array
+}
